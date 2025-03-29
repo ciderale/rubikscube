f20c77982a0a18dcf4c6d9f355c6b8ab3de1c91b
Alain Lehmann <alain.lehmann@gmail.com>
2025-03-29 17:18:34 +0100

    use stdenvNoCC

--- flake.nix
+++ flake.nix
@@ -19,8 +19,9 @@
       ];
       systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
 
-      perSystem = {
+      perSystem = {pkgs, ...}: {
         devenv.shells.default = {
+          stdenv = pkgs.stdenvNoCC;
           documentation.nix-discover.option-search.enable = true;
           documentation.nix-discover.package-search.enable = true;
           languages.texlive.enable = true;
