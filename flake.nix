e8c6e5baee85120b97da9bc9fda51aafa3459ff8
Alain Lehmann <alain.lehmann@gmail.com>
2025-03-29 17:16:44 +0100

    initial setup

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    devenv.url = "github:cachix/devenv";
+    nix-discover.url = "github:ciderale/nix-option-search/nix-discover";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+        inputs.nix-discover.modules.flake-parts-devenv
+      ];
+      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+      perSystem = {
+        devenv.shells.default = {
+          documentation.nix-discover.option-search.enable = true;
+          documentation.nix-discover.package-search.enable = true;
+          languages.texlive.enable = true;
+          languages.texlive.packages = ["standalone"];
+        };
+      };
+    };
+}
